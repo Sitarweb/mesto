88a1d542802763419cb248f40e21f3c116b67e3c
Simon Tarasenko <semeontarasenko@yandex.ru>
2022-10-23 18:17:49 +0300

    three

--- scripts/index.js
+++ scripts/index.js
@@ -5,18 +5,17 @@ const closeButton = popup.querySelector('.popup__button-close');
 
 
 const popupContent = popup.querySelector('.popup__content');
-let inputName = popup.querySelector('.form__input-name');
-let inputJob = popup.querySelector('.form__input-job');
-const saveButton = popup.querySelector('.form__button-save');
+let inputValuesName = popup.querySelector('.form__input_mean_name');
+let inputValuesJob = popup.querySelector('.form__input_mean_job');
 
 let profileName = document.querySelector('.profile__info-title');
 let profileParagraph = document.querySelector('.profile__info-paragraph');
 
 function formSubmit (evt){
     evt.preventDefault();
-    profileName.textContent = inputName.value;
-    profileParagraph.textContent = inputJob.value;
-    Closeform();
+    profileName.textContent = inputValuesName.value;
+    profileParagraph.textContent = inputValuesJob.value;
+    closeForm();
 }
 
 popupContent.addEventListener('submit', formSubmit);
@@ -25,8 +24,8 @@ openButton.addEventListener('click', function (){
     popup.classList.add('popup_is-opened');
 });
 
-function Closeform(){
+function closeForm(){
     popup.classList.remove('popup_is-opened');
 }
 
-closeButton.addEventListener('click', Closeform);
+closeButton.addEventListener('click', closeForm);
